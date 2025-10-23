003a972f74c23f9c345a09b77658116666e86a3b
nowah00 <hawon2k@gmail.com>
2025-10-23 10:54:02 +0900

    첫번째 커밋

---
+++ src/main/java/com/ssg/memberregister/MemberRegisterController.java
@@ -0,0 +1,52 @@
+package com.ssg.memberregister;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/homework/memberRegister")
+public class MemberRegisterController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        System.out.println("[MemberRegisterController - doPost OK]");
+        doHandle(req, resp);
+    }
+
+    public void doHandle(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        System.out.println("[MemberRegisterController - doHandle OK]");
+        MemberVO member = new MemberVO();
+        MemberDAO memberDAO = new MemberDAO();
+
+        request.setCharacterEncoding("UTF-8");
+        member.setUser_id(request.getParameter("user_id"));
+        member.setUser_pwd(request.getParameter("user_pwd"));
+        member.setGender(request.getParameter("gender"));
+        // String.join 기억하기!
+        member.setHobby(String.join(",", request.getParameterValues("hobby")));
+
+        System.out.println("ID : " + member.getUser_id());
+        System.out.println("PWD : " + member.getUser_pwd());
+        System.out.println("Gender : " + member.getGender());
+        System.out.println("Hobby : " + String.join(", ", request.getParameterValues("hobby")));
+
+        try {
+            int pass = memberDAO.addMember(member);
+            if(pass==1){
+                request.setAttribute("success", "회원가입 성공!");
+            } else {
+                request.setAttribute("success", "회원가입 실패..");
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/homework/result.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/main/java/com/ssg/memberregister/MemberVO.java
@@ -0,0 +1,11 @@
+package com.ssg.memberregister;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+    private String user_id;
+    private String user_pwd;
+    private String gender;
+    private String hobby;
+}
